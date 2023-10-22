c93edddc6ed4b7c63ad3d36afff847133841b6f8
Alex <alexcmilton@gmail.com>
2023-10-22 13:53:38 +1100

    added nav bar colors for all screens

--- carosel.js
+++ carosel.js
@@ -31,17 +31,56 @@ export const revealOnScroll = (currentIndex) => {
         switch (currentIndex) {
             case 0:
                 bodyHTML.css({
-                    "--nav-bar-background": "#4b4237",
-                    "--nav-bar-background-highlight": "#736b60",
-                    "--nav-bar-text-color": "#5c9ead",
+                    "--nav-bar-background": "#51291E",
+                    "--nav-bar-background-highlight": "#301014",
+                    "--nav-bar-text-color": "#7B886F",
                 });
-
                 break;
             case 1: {
                 bodyHTML.css({
                     "--nav-bar-background": "#011936",
-                    "--nav-bar-background-highlight": "#465362",
-                    "--nav-bar-text-color": "#758173",
+                    "--nav-bar-background-highlight": "#007E8F",
+                    "--nav-bar-text-color": "#465362",
+                });
+                break;
+            }
+            case 2: {
+                bodyHTML.css({
+                    "--nav-bar-background": "#4b4237",
+                    "--nav-bar-background-highlight": "#736b60",
+                    "--nav-bar-text-color": "#5c9ead",
+                });
+                break;
+            }
+            case 3: {
+                bodyHTML.css({
+                    "--nav-bar-background": "#040F0F",
+                    "--nav-bar-background-highlight": "#2D3A3A",
+                    "--nav-bar-text-color": "#CE2029",
+                });
+                break;
+            }
+            case 4: {
+                bodyHTML.css({
+                    "--nav-bar-background": "#A51080",
+                    "--nav-bar-background-highlight": "#FF2ECC",
+                    "--nav-bar-text-color": "#E0FF4F",
+                });
+                break;
+            }
+            case 5: {
+                bodyHTML.css({
+                    "--nav-bar-background": "#160202",
+                    "--nav-bar-background-highlight": "#370606",
+                    "--nav-bar-text-color": "#07EDED",
+                });
+                break;
+            }
+            default: {
+                bodyHTML.css({
+                    "--nav-bar-background": "#4b4237",
+                    "--nav-bar-background-highlight": "#736b60",
+                    "--nav-bar-text-color": "#5c9ead",
                 });
                 break;
             }
