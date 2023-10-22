e7faab7a5119975ea153bb79197f389f07993765
Alex <alexcmilton@gmail.com>
2023-10-22 13:18:45 +1100

    adding variable nav colors

--- carosel.js
+++ carosel.js
@@ -27,35 +27,23 @@ export const revealOnScroll = (currentIndex) => {
             //   item.children.item(numOfChildren - 1).classList.remove("reveal");
             $(`#image${item.id}`).get(0).classList.remove("reveal");
         }
-        const bodyHTML = $("body").get(0);
-        switch (currentIdex) {
+        const bodyHTML = $(":root");
+        switch (currentIndex) {
             case 0:
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-background",
-                    "#4b4237"
-                );
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-background-highlight",
-                    "#736b60"
-                );
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-text-color",
-                    "#5c9ead"
-                );
+                bodyHTML.css({
+                    "--nav-bar-background": "#4b4237",
+                    "--nav-bar-background-highlight": "#736b60",
+                    "--nav-bar-text-color": "#5c9ead",
+                });
+
                 break;
             case 1: {
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-background",
-                    "#011936"
-                );
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-background-highlight",
-                    "#465362"
-                );
-                bodyHTML.html.style.setProperty(
-                    "--nav-bar-text-color",
-                    "#758173"
-                );
+                bodyHTML.css({
+                    "--nav-bar-background": "#011936",
+                    "--nav-bar-background-highlight": "#465362",
+                    "--nav-bar-text-color": "#758173",
+                });
+                break;
             }
         }
     });
